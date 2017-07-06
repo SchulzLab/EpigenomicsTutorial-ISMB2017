e2745df683526a799c93eb3bcf3d860aac318930
ivan <ivan.costa@rwth-aachen.de>
2017-07-06 17:26:35 +0200

    finilization of practical step1

---
+++ session2/step1/input/download_bam.sh
@@ -0,0 +1,6 @@
+wget http://134.130.18.8/open_data/hint/ismb2017/B_ATAC.bam
+wget http://134.130.18.8/open_data/hint/ismb2017/B_ATAC.bam.bai
+wget http://134.130.18.8/open_data/hint/ismb2017/CD4_ATAC.bam
+wget http://134.130.18.8/open_data/hint/ismb2017/CD4_ATAC.bam.bai
+wget http://134.130.18.8/open_data/hint/ismb2017/LSK_ATAC.bam
+wget http://134.130.18.8/open_data/hint/ismb2017/LSK_ATAC.bam.bai
